41346bb6e6446a4ba62c064beb7f4b6dc3416a36
Matt Larkin <mattlarkin@me.com>
2015-04-16 16:58:16 -0400

    Initial Commit

---
+++ Plaid2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Plaid2
+//
+//  Created by Matt Larkin on 4/16/15.
+//  Copyright (c) 2015 Matt Larkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
